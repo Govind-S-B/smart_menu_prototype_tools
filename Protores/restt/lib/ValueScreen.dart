b73d72b371b12ec5b5edd304014a20111fb70b79
Govind-S-B <b.s.dnivog@gmail.com>
2022-11-06 07:20:56 +0530

    fixed qr

--- Protores/restt/lib/ValueScreen.dart
+++ Protores/restt/lib/ValueScreen.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:restt/HomeScreen.dart';
 
 String tableId = "";
 
@@ -34,8 +37,47 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: MobileScanner(
         onDetect: (barcode, args) {
-          debugPrint("Barcode found!" + barcode.rawValue!);
-        },
+          
+          debugPrint("Barcode Found!" + barcode.rawValue!);
+          
+          try{
+            Map decoded_qr = json.decode(barcode.rawValue!);
+          
+            debugPrint(decoded_qr["table_id"].toString());
+            
+            if (decoded_qr.containsKey("table_id")){
+              debugPrint(decoded_qr["table_id"].toString());
+              Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ScreenHome()));
+          }
+          else{
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Container(
+                height: 90,
+                decoration: BoxDecoration(color: Colors.red.shade600),
+                child: Text("Wrong QR , try again"),
+              ),
+              behavior: SnackBarBehavior.floating,
+              )
+            );
+          }
+          }
+          catch(r){
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Container(
+                height: 90,
+                decoration: BoxDecoration(color: Colors.red.shade600),
+                child: Text("Wrong QR , try again"),
+              ),
+              behavior: SnackBarBehavior.floating,
+              )
+            );
+          }
+          
+        }
       ),
     );
   }
